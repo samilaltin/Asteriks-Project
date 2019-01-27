a02ea69d31a8c74380af3989cd28816c9fc06884
Şamil Altın <samil.altin@btc-ag.com.tr>
2019-01-27 20:57:58 +0300

    check null

--- app/src/main/java/com/samilaltin/loodos/asteriksproject/MainActivity.kt
+++ app/src/main/java/com/samilaltin/loodos/asteriksproject/MainActivity.kt
@@ -2,13 +2,57 @@ package com.samilaltin.loodos.asteriksproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var activeProcess = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        btnHitMe.setOnClickListener { attemptToStart() }
+
+    }
+
+    private fun attemptToStart() {
+        if (activeProcess) {
+            return
+        }
+        txtString.error = null
+        txtAsteriks.error = null
+        var cancel = false
+        var focusView: View? = null
+
+        val string = txtString.text.toString()
+        val asteriks = txtAsteriks.text.toString()
+
+        if (TextUtils.isEmpty(string)) run {
+            txtString.error = getString(R.string.not_be_empty)
+            focusView = txtString
+            cancel = true
+        }
+
+        if (TextUtils.isEmpty(asteriks)) run {
+            txtAsteriks.error = getString(R.string.not_be_empty)
+            focusView = txtAsteriks
+            cancel = true
+        }
+
+        if (cancel) {
+            focusView?.requestFocus()
+        } else {
+            activeProcess = true
+
+            start()
+        }
+
+    }
+
+    private fun start() {
+
     }
 }
