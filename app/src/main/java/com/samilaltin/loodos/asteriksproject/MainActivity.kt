f289ae7a5b90d18d7658935a54aabfac0bdc1000
Şamil Altın <samil.altin@btc-ag.com.tr>
2019-01-27 21:42:02 +0300

    asteriks adding

--- app/src/main/java/com/samilaltin/loodos/asteriksproject/MainActivity.kt
+++ app/src/main/java/com/samilaltin/loodos/asteriksproject/MainActivity.kt
@@ -5,17 +5,19 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private var activeProcess = false
+    private var asteriks = 0
+    private val stringList = ArrayList<String>()
+    private var text = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         btnHitMe.setOnClickListener { attemptToStart() }
-
     }
 
     private fun attemptToStart() {
@@ -45,14 +47,55 @@ class MainActivity : AppCompatActivity() {
         if (cancel) {
             focusView?.requestFocus()
         } else {
-            activeProcess = true
-
+            activeProcess = false
             start()
         }
 
     }
 
     private fun start() {
+        asteriks = Integer.parseInt(txtAsteriks.text.toString())
+        val stringList = howManyRepeating()
+        setAsteriks(stringList)
+    }
 
+    private fun howManyRepeating(): ArrayList<String> {
+        val charArray = txtString!!.text.toString().toCharArray()
+        for (char in charArray) {
+            if (stringList.size == 0) {
+                val string = String(char, 1)
+                stringList.add(string)
+            } else {
+                if (stringList[stringList.size - 1].string == char) {
+                    stringList[stringList.size - 1].addAsteriks()
+                } else {
+                    val string = String(char, 1)
+                    stringList.add(string)
+                }
+            }
+        }
+        return stringList
+    }
+
+    private fun setAsteriks(stringList: ArrayList<String>) {
+        for (strings in stringList) {
+            if (strings.asteriks >= asteriks) {
+                for (i in 0 until strings.asteriks) {
+                    text += "*"
+                }
+            } else {
+                for (i in 0 until strings.asteriks) {
+                    text += strings.string
+                }
+            }
+        }
+
+        txtResult!!.text = text
+    }
+
+    class String(internal var string: Char, internal var asteriks: Int) {
+        fun addAsteriks() {
+            this.asteriks += 1
+        }
     }
 }
